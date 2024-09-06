680de7e67a823ca7d2fe980e752b7f8231296fd1
ibrahim <ibrahimcse.eng@gmail.com>
2024-09-06 23:34:02 +0600

    Add custom authentication and permission to RestaurantCreateView

--- restaurant/views.py
+++ restaurant/views.py
@@ -2,10 +2,12 @@ from .models import Restaurant
 from rest_framework.generics import ListAPIView, CreateAPIView, UpdateAPIView, DestroyAPIView
 from .serializers import RestaurantSerializer
 from authuser.models import IsStaff 
+from rest_framework.authentication import TokenAuthentication
 
 class RestaurantCreateView(CreateAPIView):
     permission_classes = [IsStaff]
     serializer_class = RestaurantSerializer
+    authentication_classes = [TokenAuthentication]
     queryset = Restaurant.objects.all()
 
 class RestaurantUpdateView(UpdateAPIView):
